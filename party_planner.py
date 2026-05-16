6d96553dd1a6697ac15134c3afadd8c29060b7b2
MrinalS24 <connectwithmrinaal@gmail.com>
2026-05-16 10:56:59 +0200

    party_planner.py

--- party_planner.py
+++ party_planner.py
@@ -14,6 +14,7 @@ name_entry = None
 no_people_spinbox = None
 guest_listbox = None
 total_people_label = None
+guest_window = None
 
 no_people = 0
 
@@ -30,6 +31,10 @@ def add_people():
     if name not in guest_list:
         guest_listbox.insert(tk.END, name)
     
+    if name in guest_list:
+        total_people -= guest_list[name][1]
+
+    total_people += no_people    
 
     guest_list[name] = [rsvp_status, no_people]
     print(guest_list)
@@ -37,7 +42,6 @@ def add_people():
     name_entry.delete(0, tk.END)
     no_people_var.set(1)
 
-    total_people += no_people
     total_people_label.config(text = f"Total No. of People: {total_people}")
 
     
@@ -53,8 +57,29 @@ def delete_people():
     total_people_label.config(text = f"Total No. of People: {total_people}")
 
 
-def update_people():
-    global guest_list, guest_listbox, total_people, total_people_label
+def show_details(event):
+    global guest_list, guest_listbox, guest_window
+
+    show_details_window = tk.Toplevel(guest_window)
+    show_details_window.geometry("100x100")
+    show_details_window.config(bg = "white")
+
+    index = guest_listbox.curselection()[0]
+
+    name = guest_listbox.get(index)
+    no_of_people = guest_list[name][1]
+    status = guest_list[name][0]
+
+    name_label = tk.Label(show_details_window, text = f"Name: {name}", font = ("Times New Roman", 13), fg = "black", bg = "white")
+    name_label.place(x = 5, y = 5)
+
+    no_people_label = tk.Label(show_details_window, text = f"No. of People: {no_of_people}", font = ("Times New Roman", 13), fg = "black", bg = "white")
+    no_people_label.place(x = 5, y = 30)
+
+    status_label = tk.Label(show_details_window, text = f"Status: {status}", font = ("Times New Roman", 13), fg = "black", bg = "white")
+    status_label.place(x = 5, y = 55)
+
+    show_details_window.mainloop()
 
 
 
@@ -70,8 +95,11 @@ def update_people():
 
 
 
-def guest_window():
-    global status_var, name_entry, no_people_spinbox, total_people, guest_listbox, total_people_label
+
+
+
+def open_guest_window():
+    global status_var, name_entry, no_people_spinbox, total_people, guest_listbox, total_people_label, guest_window
 
     guest_window = tk.Toplevel(window)
     guest_window.title("Guest List Window")
@@ -80,6 +108,11 @@ def guest_window():
 
     guest_listbox = tk.Listbox(guest_window, width = 30, height = 20, font = ("Times New Roman", 15), fg = "black", bg = "white")
     guest_listbox.place(x = 20, y = 20)
+
+    guest_listbox.bind("<<ListboxSelect>>", show_details)
+
+    for i in guest_list.keys():
+        guest_listbox.insert(tk.END, i)
 
     name_label = tk.Label(guest_window, text = "Enter Guest Name:", fg = "black", bg = "#FDFD96", font = ("Times New Roman", 18))
     name_label.place(x = 280, y = 30)
@@ -95,11 +128,8 @@ def guest_window():
     accepted_button = tk.Radiobutton(guest_window, text = "Confirmed", fg = "black", bg = "#FDFD96", variable = status_var, value = "Confirmed", font = ("Times New Roman", 18))
     accepted_button.place(x = 280, y = 155)
 
-    declined_button = tk.Radiobutton(guest_window, text = "Declined", fg = "black", bg = "#FDFD96", variable = status_var, value = "Declined", font = ("Times New Roman", 18))
-    declined_button.place(x = 280, y = 185)
-
     unconfirmed_button = tk.Radiobutton(guest_window, text = "Unconfirmed", fg = "black", bg = "#FDFD96", variable = status_var, value = "Unconfirmed", font = ("Times New Roman", 18))
-    unconfirmed_button.place(x = 280, y = 215)
+    unconfirmed_button.place(x = 280, y = 190)
 
     no_people_label = tk.Label(guest_window, text = "No. of People:", fg = "black", bg = "#FDFD96", font = ("Times New Roman", 18))
     no_people_label.place(x = 280, y = 265)
@@ -110,11 +140,8 @@ def guest_window():
     add_button = tk.Button(guest_window, text = "Add", fg = "black", bg = "white", bd = 4, relief = tk.GROOVE, font = ("Times New Roman", 18), command = add_people)
     add_button.place(x = 280, y = 360)
 
-    update_button = tk.Button(guest_window, text = "Update", fg = "black", bg = "white", bd = 4, relief = tk.GROOVE, font = ("Times New Roman", 18))
-    update_button.place(x = 380, y = 360)
-
     delete_button = tk.Button(guest_window, text = "Delete", fg = "black", bg = "white", bd = 4, relief = tk.GROOVE, font = ("Times New Roman", 18), command = delete_people)
-    delete_button.place(x = 330, y = 410)
+    delete_button.place(x = 380, y = 360)
 
     total_people_label = tk.Label(guest_window, text = f"Total No. of People: {total_people}", fg = "black", bg = "#FDFD96", bd = 2, relief = tk.SUNKEN, font = ("Times New Roman", 20), width = 22)
     total_people_label.place(x = 28, y = 420)
@@ -139,11 +166,19 @@ window.title("Party Planner")
 
 no_people_var = tk.IntVar(value = 1)
 
+streamer_img = tk.PhotoImage(file = "streamers.png") #loading images
+money_img = tk.PhotoImage(file = "money.png")
+
+streamer_img_label = tk.Label(window, image = streamer_img, bg = "#ffd1f4")
+streamer_img_label.pack()
+
+
+
 
 planner_label = tk.Label(window, text = "PARTY PLANNER", fg = "black", bg = "#ffd1f4", font = ("Times New Roman", 20))
 planner_label.place(x = 230, y = 230)
 
-guest_list_button = tk.Button(window, text = "GUEST LIST", fg = "black", bg = "white", font = ("Times New Roman", 20), command = guest_window)
+guest_list_button = tk.Button(window, text = "GUEST LIST", fg = "black", bg = "white", font = ("Times New Roman", 20), command = open_guest_window)
 guest_list_button.place(x = 235, y = 80)
 
 checklist_button = tk.Button(window, text = "CHECKLIST", fg = "black", bg = "white", font = ("Times New Roman", 20), command = checklist_window)
@@ -154,6 +189,8 @@ budget_button.place(x = 15, y = 230)
 
 food_button = tk.Button(window, text = "MENU", fg = "black", bg = "white", font = ("Times New Roman", 20), command = menu_window)
 food_button.place(x = 480, y = 230)
+
+# to add an image in a button, add a property "image" and put in the image variable as loaded onto the file. 
 
 
 
