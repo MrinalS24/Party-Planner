ce6c95d421c9b39dc5e62868edeb19aaf7600464
MrinalS24 <connectwithmrinaal@gmail.com>
2026-05-23 10:07:30 +0200

    menu.py

--- menu.py
+++ menu.py
@@ -2,11 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 
 
+
 food_listbox = None
 
 food_entry = None
 food_type_var = None
 v_nv_type_var = None
+veg_button = None
+non_veg_button = None
 
 food_list = []
 
@@ -14,11 +17,13 @@ food_list = []
 
 
 def add_food_item():
-    global food_entry, food_type_var, v_nv_type_var, food_listbox
+    global food_entry, food_listbox, non_veg_button, veg_button, food_type_var, v_nv_type_var
 
     food_name = food_entry.get()
     food_type = food_type_var.get()
     v_nv = v_nv_type_var.get()
+
+    print(v_nv)
 
     food_enter_lisbox = (food_name, food_type, v_nv)
 
@@ -27,6 +32,9 @@ def add_food_item():
     final_of_food_list = len(food_list) -1
 
     food_listbox.insert(tk.END, food_list[final_of_food_list])
+
+
+
 
 
 
@@ -49,11 +57,11 @@ def remove_food_item():
 
 
 
-def menu_window():
-    global food_entry, food_type_var, v_nv_type_var, food_listbox, food_list
+def open_menu_window():
+    global food_entry, v_nv_type_var, food_listbox, food_list, food_type_var, veg_button, non_veg_button
 
-    menu_window = tk.Tk()
-    menu_window.title("Menu WIndow")
+    menu_window = tk.Toplevel()
+    menu_window.title("Menu Window")
     menu_window.geometry("500x500")
     menu_window.config(bg = "#ACE1AF")
 
@@ -70,7 +78,8 @@ def menu_window():
     food_entry = tk.Entry(menu_window, bg = "white", fg = "black", bd = 3, relief = tk.GROOVE, font = ("Times New Roman", 18))
     food_entry.place(x = 270, y = 60)
 
-    food_type_var = tk.StringVar(value = "Mains")
+    global food_type_var
+    food_type_var = tk.StringVar(value = "Drinks")
 
     drink_button = tk.Radiobutton(menu_window, text = "Drinks", variable = food_type_var, value = "Drinks", bg = "#ACE1AF", fg = "black", font = ("Times New Roman", 18))
     drink_button.place(x = 270, y = 100)
@@ -84,12 +93,13 @@ def menu_window():
     dessert_button = tk.Radiobutton(menu_window, text = "Dessert", variable = food_type_var, value = "Dessert", bg = "#ACE1AF", fg = "black", font = ("Times New Roman", 18))
     dessert_button.place(x = 270, y = 190)
 
-    v_nv_type_var = tk.StringVar(value = "Veg")
+    global v_nv_type_var
+    v_nv_type_var = tk.StringVar(value="Veg")
 
-    veg_button = tk.Radiobutton(menu_window, text = "Vegetarian", variable = v_nv_type_var, value = "Veg", bg = "#ACE1AF", fg = "black", font = ("Times New Roman", 18))
+    veg_button = tk.Radiobutton(menu_window, text = "Vegetarian", value = "Veg", variable = v_nv_type_var,bg = "#ACE1AF", fg = "black", font = ("Times New Roman", 18))
     veg_button.place(x = 270, y = 250)
 
-    non_veg_button = tk.Radiobutton(menu_window, text = "Non-Veg", variable = v_nv_type_var, value = "Non-Veg", bg = "#ACE1AF", fg = "black", font = ("Times New Roman", 18))
+    non_veg_button = tk.Radiobutton(menu_window, text = "Non-Veg", value = "Non-Veg", variable = v_nv_type_var, bg = "#ACE1AF", fg = "black", font = ("Times New Roman", 18))
     non_veg_button.place(x = 270, y = 280)
 
     add_button = tk.Button(menu_window, text = "Add Food Item", bg = "white", fg = "black", bd = 4, relief = tk.SUNKEN, font = ("Times New Roman", 18), command = add_food_item)
@@ -97,6 +107,8 @@ def menu_window():
 
     remove_button = tk.Button(menu_window, text = "Remove Food Item", bg = "white", fg = "black", bd = 4, relief = tk.SUNKEN, font = ("Times New Roman", 18), command = remove_food_item)
     remove_button.place(x = 270, y = 400)
+
+
 
     
 
